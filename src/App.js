b3e852df3170a76a0052e82f0e58fcd3e51073f4
mw3981 <mw3981@nyu.edu>
2022-03-08 20:42:52 -0500

    adding text content draft

--- src/App.js
+++ src/App.js
@@ -1,7 +1,60 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <section className="Hero">
+        <h1>Filter Dysmorphia</h1>
+      </section>
+      <section className="Overview">
+        <h2>Overview</h2>
+        <p>
+          Face filters have dominated social media platforms in recent years.
+          Popular celebrities and regular social media users alike have shared
+          videos of themselves using these filters. Let’s take a step back and
+          learn the origin of these filters. In 2015, Snapchat introduced a
+          collection of filters called “Lenses” which included puppy ears to
+          barfing rainbows. Instagram and Facebook created their own collection
+          of filters in 2017, cementing the concept of filters being a key part
+          of social media usage. During the same year, Snapchat’s in-house team
+          built Snapchat Lens Studio and Facebook introduced Spark AR Studio.
+          Both of these softwares gave users the tools necessary to create their
+          own filters. In 2018, these 3 big companies began allowing third
+          parties to develop face filters on their platform for public use.
+          Since then, various AR artists on social media have started sharing
+          their own filters.
+        </p>
+      </section>
+      <section>
+        <h2>Data</h2>
+        <p>
+          77% of American Academy of Facial Plastic and Reconstructive Surgery
+          members reported patients seeking cosmetic procedures with “looking
+          better in selfies” as a driving factor for patients. This was an
+          increase of up to 35% overall since the AAFPRS first identified this
+          trend in 2016.
+        </p>
+        <p>
+          90% of the 175 young women and nonbinary people from the study
+          reported using filters when posting a selfie. Of these, more than half
+          reported using a filter half the time or more.
+        </p>
+        <p>
+          85% of women reported using filters and editing tools for these most
+          popular edits:
+          <ul>
+            <li>brighten/bronze skin</li>
+            <li>reshape nose or jaw</li>
+            <li>look slimmer</li>
+            <li>make lips fuller</li>
+            <li>whiten teeth</li>
+          </ul>
+        </p>
+        <p>90% felt pressure to look attractive</p>
+        <p>75% said that they would “never live up to the images they see”</p>
+      </section>
+    </div>
+  );
 }
 
 export default App;
