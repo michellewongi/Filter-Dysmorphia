65b3aaf9303e7371e68fe39b6ce6cdef61c53034
Michelle Wong <mw3981@nyu.edu>
2022-05-11 01:00:13 -0400

    adding QR codes for filters

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -8,6 +8,9 @@ import { default as filter1 } from "../assets/filter1.png";
 import { default as filter2 } from "../assets/filter2.png";
 import { default as filter3 } from "../assets/filter3.png";
 import { default as filterMeshBase } from "../assets/baseMesh.png";
+import { default as QRFilter1 } from "../assets/QR_Filter1.png";
+import { default as QRFilter2 } from "../assets/QR_Filter2.png";
+import { default as QRFilter3 } from "../assets/QR_Filter3.png";
 
 export default function Filters() {
   return (
@@ -38,15 +41,20 @@ export default function Filters() {
           filter is changing. The purpose of creating this filter was to see
           what changes are being made to what parts of your face.
         </p>
-        <h4 className="FilterListHeader">Edits made:</h4>
-        <ul className="FilterList">
-          <li>Slimmer jaw</li>
-          <li>Smaller nose</li>
-          <li>Lifting the eyes</li>
-          <li>Larger lips</li>
-          <li>Sharper chin</li>
-          <li>Retouched skin</li>
-        </ul>
+        <div className="FilterDetailsRow">
+          <div>
+            <h4 className="FilterListHeader">Edits made:</h4>
+            <ul className="FilterList">
+              <li>Slimmer jaw</li>
+              <li>Smaller nose</li>
+              <li>Lifting the eyes</li>
+              <li>Larger lips</li>
+              <li>Sharper chin</li>
+              <li>Retouched skin</li>
+            </ul>
+          </div>
+          <img src={QRFilter1} className="QRCode" />
+        </div>
 
         <h3 className="FirstFilterHeader">filter#2</h3>
 
@@ -71,16 +79,20 @@ export default function Filters() {
           "anti-beauty" beauty filter would look like. Most of my editing was
           focused on dragging the face down.
         </p>
-        <h4 className="FilterListHeader">Edits made:</h4>
-        <ul className="FilterList">
-          <li>Wider jaw</li>
-          <li>Drooping eyes</li>
-          <li>Smaller lips</li>
-          <li>Wider chin</li>
-        </ul>
+        <div className="FilterDetailsRow">
+          <div>
+            <h4 className="FilterListHeader">Edits made:</h4>
+            <ul className="FilterList">
+              <li>Wider jaw</li>
+              <li>Drooping eyes</li>
+              <li>Smaller lips</li>
+              <li>Wider chin</li>
+            </ul>
+          </div>
+          <img src={QRFilter2} className="QRCode" />
+        </div>
 
         <h3 className="FirstFilterHeader">filter#3</h3>
-
         <div className="FilterWrapper">
           <div className="FilterItem">
             <img src={filterMeshBase} className="FilterImg" />
@@ -101,15 +113,21 @@ export default function Filters() {
           curious what an extreme version of existing beauty filters look like
           and I created a sort of botched surgery filter.
         </p>
-        <h4 className="FilterListHeader">Edits made:</h4>
-        <ul className="FilterList">
-          <li>Wider jaw</li>
-          <li>Smaller nose</li>
-          <li>Lifting the eyes</li>
-          <li>Larger lips</li>
-          <li>Sharper chin</li>
-          <li>Prominent cheekbones</li>
-        </ul>
+
+        <div className="FilterDetailsRow">
+          <div>
+            <h4 className="FilterListHeader">Edits made:</h4>
+            <ul className="FilterList">
+              <li>Wider jaw</li>
+              <li>Smaller nose</li>
+              <li>Lifting the eyes</li>
+              <li>Larger lips</li>
+              <li>Sharper chin</li>
+              <li>Prominent cheekbones</li>
+            </ul>
+          </div>
+          <img src={QRFilter3} className="QRCode" />
+        </div>
       </section>
     </>
   );
